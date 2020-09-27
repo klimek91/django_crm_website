a81d292d9bf1d07cb916a6c00fe67530e24b7e4f
Klimek <klimkowskib@gmail.com>
2020-09-27 18:57:01 +0200

    create admin, create 3 models added to admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out door', 'Out door'),
+    )
+    name = models.CharField(max_length=100, null=True)
+    price = models.FloatField(null = True)
+    category = models.CharField(max_length=100, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=100, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    category = models.CharField(max_length=100, null=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
