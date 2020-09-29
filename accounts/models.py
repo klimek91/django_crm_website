b75e9d07b0b90e7fce255ee58d914c55aa4533ec
Klimek <klimkowskib@gmail.com>
2020-09-29 21:09:50 +0200

    add tag model, create database relationships

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,11 @@ class Customer(models.Model):
     phone = models.CharField(max_length=100, null=True)
     email = models.CharField(max_length=100, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    def __str__(self):
+        return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, null=True)
     def __str__(self):
         return self.name
 
@@ -19,8 +23,11 @@ class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     price = models.FloatField(null = True)
     category = models.CharField(max_length=100, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=100, null=True)
+    description = models.CharField(max_length=100, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUS = (
@@ -30,3 +37,6 @@ class Order(models.Model):
     )
     category = models.CharField(max_length=100, null=True, choices=STATUS)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+
