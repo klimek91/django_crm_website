e9932c4168c920ad859167d513f6c0e4fa37b1ee
Klimek <klimkowskib@gmail.com>
2020-10-31 18:37:01 +0100

    filter form table search

--- accounts/models.py
+++ accounts/models.py
@@ -35,10 +35,11 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    status = models.CharField(max_length=100, null=True, choices=STATUS)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=100, null=True, choices=STATUS)
+    note = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.product.name
